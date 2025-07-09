2e21e60e437acec040e45ae6ec7c5378927cd48a
Sriram Kumar <skishore@amd.com>
2025-07-10 00:51:50 +0300

    Cherry-picked commit with merge conflict

--- csrc/megatron/scaled_masked_softmax.h
+++ csrc/megatron/scaled_masked_softmax.h
@@ -23,6 +23,10 @@
 #include <stdint.h>
 #include <cuda_fp16.h>
 #include <c10/macros/Macros.h>
+<<<<<<< HEAD
+=======
+#include <ATen/cuda/CUDAContext.h>
+>>>>>>> 7d9b032 (Fixing the C10_warpsize issue. replacing the macros with at::cuda::warp_size() (#237))
 
 namespace {
 
@@ -437,7 +441,12 @@ int get_batch_per_block(int query_seq_len, int key_seq_len, int batches, int att
     int log2_elements = log2_ceil(key_seq_len);
     const int next_power_of_two = 1 << log2_elements;
 
+<<<<<<< HEAD
     int warp_size = (next_power_of_two < C10_WARP_SIZE) ? next_power_of_two : C10_WARP_SIZE;
+=======
+    int warp_size = (next_power_of_two < at::cuda::warp_size()) ? next_power_of_two : at::cuda::warp_size();
+
+>>>>>>> 7d9b032 (Fixing the C10_warpsize issue. replacing the macros with at::cuda::warp_size() (#237))
     int batches_per_warp = (next_power_of_two <= 128) ? 2 : 1;
 
     constexpr int threads_per_block = 128;
@@ -466,7 +475,11 @@ void dispatch_scaled_softmax_forward(
         int batch_count = batches * attn_heads * query_seq_len;
 
         // This value must match the WARP_SIZE constexpr value computed inside softmax_warp_forward.
+<<<<<<< HEAD
         int warp_size = (next_power_of_two < C10_WARP_SIZE) ? next_power_of_two : C10_WARP_SIZE;
+=======
+        int warp_size = (next_power_of_two < at::cuda::warp_size()) ? next_power_of_two : at::cuda::warp_size();
+>>>>>>> 7d9b032 (Fixing the C10_warpsize issue. replacing the macros with at::cuda::warp_size() (#237))
 
         // This value must match the WARP_BATCH constexpr value computed inside softmax_warp_forward.
         int batches_per_warp = (next_power_of_two <= 128) ? 2 : 1;
@@ -568,7 +581,11 @@ void dispatch_scaled_masked_softmax_forward(
         int batch_count = batches * attn_heads * query_seq_len;
 
         // This value must match the WARP_SIZE constexpr value computed inside softmax_warp_forward.
+<<<<<<< HEAD
         int warp_size = (next_power_of_two < C10_WARP_SIZE) ? next_power_of_two : C10_WARP_SIZE;
+=======
+        int warp_size = (next_power_of_two < at::cuda::warp_size()) ? next_power_of_two : at::cuda::warp_size();
+>>>>>>> 7d9b032 (Fixing the C10_warpsize issue. replacing the macros with at::cuda::warp_size() (#237))
 
         // This value must match the WARP_BATCH constexpr value computed inside softmax_warp_forward.
         int batches_per_warp = (next_power_of_two <= 128) ? 2 : 1;
