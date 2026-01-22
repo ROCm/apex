9b104d01eafd78ed5994ddc1df37b699c11b3e9b
Mikko Tukiainen <mikko.tukiainen@amd.com>
2026-01-22 17:01:20 +0200

    omit check_cudnn_version_and_warn check (no cuDNN on ROCm)

--- apex/contrib/conv_bias_relu/conv_bias_relu.py
+++ apex/contrib/conv_bias_relu/conv_bias_relu.py
@@ -1,13 +1,10 @@
 import torch
 from torch.autograd import gradcheck
 
-if check_cudnn_version_and_warn(__name__, 8400):
-    try:
-        import fused_conv_bias_relu
-    except ImportError:
-        fused_conv_bias_relu = None
-else:
-    fused_conv_bias_relu = None
+try:
+   import fused_conv_bias_relu
+except ImportError:
+   fused_conv_bias_relu = None
 
 
 class ConvBiasReLU_(torch.autograd.Function):
