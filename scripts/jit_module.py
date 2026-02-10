a3635d39212c09e3a30e0a9982fac4423d7832a5
skishore <sriramkumar.kishorekumar@amd.com>
2026-02-10 14:40:15 +0000

    add code to create a builder based on user inputs

--- scripts/jit_module.py
+++ scripts/jit_module.py
@@ -29,6 +29,18 @@ class JitModule:
         return f"_{new_name}Module"
 
 
+    def create_builder_class_name(self, module_name):
+        parts = module_name.split("_")
+        new_name = ""
+        for part in parts:
+            new_name += part.capitalize()
+        return f"{new_name}Builder"
+
+    
+    def create_build_var(self, module_name):
+        return f"APEX_BUILD_{module_name.upper()}"
+
+
     def check_if_builder_module_exists(self, module_name):
         if os.path.exists(os.path.join(self.op_builder_folder, f"{module_name}.py")):
             return True
@@ -53,20 +65,8 @@ class JitModule:
         return None
 
 
-    def add_jit_module(self, builder_name):
-
-        is_builder_exists = self.check_if_builder_module_exists(builder_name)
-        if not is_builder_exists:
-            print(f"Builder module {builder_name} does not exist")
-            return
-
-        #get module name from builder name
-        module_name = self.get_module_name(builder_name)
-        if module_name is None:
-            print(f"Module name for builder {builder_name} not found")
-            return
-        print(f"Module name for builder {builder_name} is {module_name}")
-
+    def create_loader(self, builder_name):
+        module_name = self.get_module_name(builder_name) or builder_name
         #check if a loader module in compatability folder
         is_loader_exists = self.check_if_loader_module_exists(module_name)
         if is_loader_exists:
@@ -120,13 +120,107 @@ class JitModule:
             f.write(f"sys.modules[__name__] = {loader_class_name}()\n")
 
         print(f"Loader module {module_name} created")
-        return True
+
+
+    def create_builder(self, module_name):
+        #Interactively prompt for builder details and create the builder module.
+        if_cuda_module = input("Is this a CUDA module? (Y/n) ").strip() or "y"
+        sources = input("Enter the sources (comma separated) ").strip()
+
+
+        if if_cuda_module == "y":
+            class_name = "CUDAOpBuilder"
+            include_flag = "APEX_BUILD_CUDA_OPS"
+        else:
+            class_name = "CPUOpBuilder"
+            include_flag = "APEX_BUILD_CPU_OPS"
+
+        builder_class_name = self.create_builder_class_name(module_name)
+        build_var = self.create_build_var(module_name)
+        
+        if len(sources) == 0:
+            sources_list = []
+            sources_list_string = "[]"
+        else:
+            sources_list = sources.split(",")
+            sources_list_string = "[" + ",".join(["'" + source.strip() + "'" for source in sources_list]) + "]"
+        print(f"sources_list_string: {sources_list_string}")
+
+        include_paths = []
+        for source in sources_list:
+            if "csrc" in source and "csrc" not in include_paths:
+                include_paths.append("csrc")
+            elif "contrib/csrc" in source and "contrib/csrc" not in include_paths:
+                include_paths.append("contrib/csrc")
+        include_paths_string = "[" + ",".join(["'" + path.strip() + "'" for path in include_paths]) + "]"
+
+        with open(os.path.join(self.op_builder_folder, f"{module_name}.py"), "w") as f:
+            if if_cuda_module == "y":
+                f.write(f"from .builder import CUDAOpBuilder\n")
+            else:
+                f.write(f"from .builder import CPUOpBuilder\n")
+            f.write(f"\n")
+            f.write(f"class {builder_class_name}({class_name}):\n")
+            f.write(f"    BUILD_VAR = \"{build_var}\"\n")
+            f.write(f"    INCLUDE_FLAG = \"{include_flag}\"\n")
+            f.write(f"    NAME = \"{module_name}\"\n")
+            f.write(f"\n")
+            f.write(f"    def __init__(self):\n")
+            f.write(f"        super().__init__(name=self.NAME)\n")
+            f.write(f"\n")
+            f.write(f"    def absolute_name(self):\n")
+            f.write(f"        return f'apex.{{self.NAME}}'\n")
+            f.write(f"\n")  
+            f.write(f"    def sources(self):\n")
+            f.write(f"        return {sources_list_string}\n")  
+            f.write(f"\n")
+            f.write(f"    def include_paths(self):\n")
+            f.write(f"        return {include_paths_string}\n")
+            f.write(f"\n")
+            f.write(f"    def cxx_args(self):\n")
+            f.write(f"        return super().cxx_args() + self.generator_args() + self.version_dependent_macros()\n")
+            f.write(f"\n")
+            f.write(f"    def nvcc_args(self):\n")  
+            f.write(f"        return super().nvcc_args() + ['-O3'] + self.version_dependent_macros()\n")
+            f.write(f"\n")
+            f.write(f"    def is_compatible(self, verbose=False):\n")
+            f.write(f"        return True\n")
+            f.write(f"\n")
+            f.write(f"    def libraries_args(self):\n")
+            f.write(f"        return self.libraries_args()\n")
+
+        print(f"Builder module {builder_name} created")
+
+
+    def add_jit_module(self, builder_name):
+        #check if builder module exists
+        is_builder_exists = self.check_if_builder_module_exists(builder_name)
+        if not is_builder_exists:
+            self.create_builder(builder_name)
+        else:
+            print(f"Builder module {builder_name} already exists")
+
+        #get module name from builder name
+        module_name = self.get_module_name(builder_name)
+        if module_name is None:
+            print(f"Module name for builder {builder_name} not found")
+            return
+
+        #if the loader module does not exist, create it
+        if not self.check_if_loader_module_exists(builder_name):
+            self.create_loader(builder_name)
 
 
 def main():
     jit_module = JitModule()
-    builder_name = sys.argv[1]
-    success = jit_module.add_jit_module(builder_name)
+    if len(sys.argv) > 1:
+        module_name = sys.argv[1]
+    else:
+        module_name = input("What is the name of the module? ").strip()
+    if not module_name:
+        print("No module name provided.")
+        sys.exit(1)
+    success = jit_module.add_jit_module(module_name)
     if success:
         print("JIT module added")
 
